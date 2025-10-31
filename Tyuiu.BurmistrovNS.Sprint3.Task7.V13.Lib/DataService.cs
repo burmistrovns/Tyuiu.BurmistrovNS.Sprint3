2bcd13d466c6747caf929a903574decd46b892ef
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-11-01 00:33:26 +0500

    Task7

---
+++ Tyuiu.BurmistrovNS.Sprint3.Task7.V13.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.BurmistrovNS.Sprint3.Task7.V13.Lib
+{
+    public class DataService : ISprint3Task7V13
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double denominator = Math.Cos(x) + 1;
+
+                if (Math.Abs(denominator) < 0.0001)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = 3 * x + 2 - (x / denominator);
+                }
+
+                valueArray[count] = Math.Round(y, 2);
+
+                count++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint3.Task7.V13/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BurmistrovNS.Sprint3.Task7.V13.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint3.Task7.V13
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.Write("Начало: " + startValue);
+            Console.Write("Конец: " + stopValue);
+
+  
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+            int len = res.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine("X = {0,3} | F(x) = {1,7:F2}", startValue + i, res[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+
+
+
+}
