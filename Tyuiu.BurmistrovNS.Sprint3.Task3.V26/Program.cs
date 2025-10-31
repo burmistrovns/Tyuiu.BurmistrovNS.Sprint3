b3ed61e9197a8712c702f9eec81b6d134d309a9b
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-10-31 21:14:09 +0500

    Task3

---
+++ Tyuiu.BurmistrovNS.Sprint3.Task3.V26/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BurmistrovNS.Sprint3.Task3.V26.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint3.Task3.V26
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "have a nice time";
+            char chr = 'e';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Исходный символ = " + chr);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество символов = " + ds.GetCharCount(value, chr));
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+
+
+
+}
