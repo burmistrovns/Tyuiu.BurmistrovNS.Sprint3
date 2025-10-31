d9942543f57100d90e2df284c237d6b9ceca7227
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-10-31 20:56:14 +0500

    Task2

---
+++ Tyuiu.BurmistrovNS.Sprint3.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BurmistrovNS.Sprint3.Task2.V25.Lib;
+
+namespace Tyuiu.BurmistrovNS.Sprint3.Task2.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 13;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 16.016;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint3.Task2.V25/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BurmistrovNS.Sprint3.Task2.V25.Lib;
+
+namespace Tyuiu.BurmistrovNS.Sprint3.Task2.V25
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 13;
+
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Стaрт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+
+
+
+}
