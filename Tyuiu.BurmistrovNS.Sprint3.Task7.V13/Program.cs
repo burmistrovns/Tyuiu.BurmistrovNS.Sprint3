5fe67af0d283e37dbf284c265585e42234439eca
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-11-01 00:47:40 +0500

    Task7

--- Tyuiu.BurmistrovNS.Sprint3.Task7.V13/Program.cs
+++ Tyuiu.BurmistrovNS.Sprint3.Task7.V13/Program.cs
@@ -14,21 +14,30 @@ namespace Tyuiu.BurmistrovNS.Sprint3.Task7.V13
             int startValue = -5;
             int stopValue = 5;
 
-            Console.Write("Начало: " + startValue);
-            Console.Write("Конец: " + stopValue);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
 
-  
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            double[] res = ds.GetMassFunction(startValue, stopValue);
-            int len = res.Length;
-
-            for (int i = 0; i < len; i++)
+            Console.WriteLine("+-------------+------------+");
+            Console.WriteLine("|      X     |    f(x)     |");
+            Console.WriteLine("+-------------+------------+");
+            for (int i = 0; i <= len - 1; i++)
             {
-                Console.WriteLine("X = {0,3} | F(x) = {1,7:F2}", startValue + i, res[i]);
+                Console.WriteLine("|{0,7:d}     |  {1, 7:f2}    |", startValue, valueArray[i]);
+                startValue++;
             }
+            Console.WriteLine("+------------+-------------+");
 
             Console.ReadKey();
 
